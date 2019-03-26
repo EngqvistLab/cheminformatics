6fb75c680068424684289bb00ae443ad80ec9224
Emma Rydholm <emmarydholm@hotmail.com>
2019-03-26 12:01:52 +0100

    Generates a dataframe with different similarity scoresfor all combinations of fingerprints. Different fingerprint algorithms are available.

--- code/similarity_score.py
+++ code/similarity_score.py
@@ -3,25 +3,38 @@ from rdkit.Chem import AllChem
 from rdkit import DataStructs
 import itertools
 import pandas as pd
+from rdkit.Chem import MACCSkeys
+from rdkit.Chem.Fingerprints import FingerprintMols
+from rdkit.Chem.AtomPairs import Pairs
 
 
 
-smile = ["CCCCCCCCCCCCCCC(C(=O)[O-])O", "[O-]C(=O)C(O)CCCCCCCCCC", "C[C@@H](C(=O)[O-])O", "C(C(=O)[O-])O","CCCCCCCCCCCCCCC(C(=O)[O-])O"]
-
-def tanimoto(smile):
+def similarity(smile, fingerprint_algorithm = "morgan"):
 
     """
-    Input: a list containing smiles.
+    Input: a list containing smiles and fingerprint algorithm: morgan (default), MACCS or topological.
     Returns: A dataframe containing calculated tanimoto, dice, cosine score between every combination of the fingerprints.
     
     """
     
     smile_fp = {}
     
+    # Calculates the choosen fingerprint.
+    
     for i in smile:
     
         mol=Chem.MolFromSmiles(i)
-        smile_fp[i] = AllChem.GetMorganFingerprintAsBitVect(mol,5)
+        
+        if fingerprint_algorithm == "morgan" :
+            smile_fp[i] = AllChem.GetMorganFingerprintAsBitVect(mol,5)
+        elif fingerprint_algorithm == "maccs": 
+            smile_fp[i] = MACCSkeys.GenMACCSKeys(mol)
+        elif fingerprint_algorithm == "topological":
+            smile_fp[i] = FingerprintMols.FingerprintMol(mol)
+        else:
+            raise NotImplementedError("Not implemented. Available fingerprint algorithms: morgan, maccs, topological.")
+            
+    # Calculates tanimoto, dice and cosine score for each combination of fingerprints and makes a dataframe.
     
     index = pd.MultiIndex.from_tuples(list(itertools.combinations(smile_fp, 2)), names=['Molecule x', 'Molecule Y'])
     similarity_df = pd.DataFrame(index=index, columns=['Tanimoto','Dice','Cosine'])
