1a001a30224f38df3c189a1b10e016744b35e442
David Högberg <david.hogberg@live.se>
2019-04-02 09:44:26 +0200

    edit file rdkit-fingerprints.py

--- code/fingerprints/rdkit-fingerprints.py
+++ code/fingerprints/rdkit-fingerprints.py
@@ -1,51 +1,27 @@
-"""
-Converts an input file with smiles-strings to an output file with fingerprints
-using RDKit. Three command line arguments specify the input file, the output
-file and the type of fingerprint, respectively. The fingerprint formats can be
-either 'morgan', 'topological' och 'maccs'.
-"""
-
-import sys
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem.Fingerprints import FingerprintMols
 from rdkit.Chem import MACCSkeys
 
-if len(sys.argv) != 4:
-    print("Not enough input arguments")
-    print("Specify input file, output file and fingerprint format")
-    exit()
+descriptors = {
+    'maccs':       lambda m: MACCSkeys.GenMACCSKeys(m),
+    'morgan3':     lambda m: AllChem.GetMorganFingerprintAsBitVect(m,3),
+    'morgan5':     lambda m: AllChem.GetMorganFingerprintAsBitVect(m,5),
+    'rdkit':       lambda m: FingerprintMols.FingerprintMol(m)
+}
 
-inFile = sys.argv[1]    # input file with smiles
-outFile = sys.argv[2]   # output file with fingerprints
-format = sys.argv[3]    # morgan, topological or maccs
+"""
+Function that calculates fingerprints for a list of smiles strings. The
+fingerprint formats can be chosen from the list of descriptors.
+"""
+def fingerprint(smiles, fingerprint):
+    if fingerprint not in descriptors:
+        raise ValueError('Invalid descriptor name ' + descriptor)
 
-if format not in ['morgan', 'topological', 'maccs']:
-    print('Not a valid fingerprint format')
-    exit()
+    # make a list of mols
+    ms = [Chem.MolFromSmiles(x) for x in smiles]
 
-# fingerprint function depending on the chosen format
-def fingerprint(format):
-    if format == 'morgan':
-        fp = AllChem.GetMorganFingerprintAsBitVect(m,5)
-        # default 2048 number of bits
-    elif format == 'topological':
-        fp = FingerprintMols.FingerprintMol(m)
-    elif format == 'maccs':
-        fp = MACCSkeys.GenMACCSKeys(m)
-    else:
-        print('Not a valid fingerprint format')
-        exit()
-    return fp
-
-i = open(inFile,'r')
-o = open(outFile,'w')
-
-for line in i:
-    s = line.rstrip('\n')
-    m = Chem.MolFromSmiles(s)
-    fp = fingerprint(format) # not a bit string
-    o.write(fp.ToBitString() + '\n')
-
-i.close()
-o.close()
+    # make a list of fingerprints
+    fingerprint = descriptors[descriptor] # fingerprint type
+    fps = [fingerprint(x) for x in ms]
+    return fps
