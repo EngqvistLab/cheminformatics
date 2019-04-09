3d4477a0829173633aaf12db0d745c638112c436
Emma Rydholm <emmarydholm@hotmail.com>
2019-04-09 13:44:20 +0200

    Removed the plot from Butina and DBSCAN clustering functions

--- code/dbscan_cluster.py
+++ code/dbscan_cluster.py
@@ -1,24 +1,12 @@
 from rdkit import Chem
 from rdkit.Chem import MACCSkeys
-from sklearn.decomposition import PCA
-from sklearn.manifold import TSNE
 from sklearn.cluster import DBSCAN
-import matplotlib.pyplot as plt
 import numpy as np
 
-def dbscan_cluster(smile, eps=0.2, min_samples=2, perplexity=5.0, learning_rate=500.0,n_iter=5000):
+def dbscan_clustering(smile, eps=0.3, min_samples=2):
     """
     Input: a list containing smiles.
-    Output: DBSCAN cluster for the molecules, visualised with colours in a 2D plot and 
-    a list with the smiles of each cluster.
-    Data processing: The data have first been processed first with PCA down to 50 dimensions 
-    (or less if the number of smiles is less) and then with tSNE, down to 2 dimensions. 
-    The result after tSNE will be plotted. To achive a nice looking plot, 
-    the parameters perplexity and learning rate can be changed to fit the dataset.
-    Input to tSNE:
-    perplexity: A approximation on how many close neighbours for each point.
-    n_iter: maximum numbers of iterations
-    learning_rate: Usually in the rang [10:1000]
+    Output: DBSCAN cluster for the molecules, and a list with the smiles of each cluster.
     DBSCAN clustering: DBSCAN clustering algorithm from scikit learn is used on the 
     fingerprint vectors, with Jaccard as the pairwise distance metric. 
     The different clusters are visualised with different colours in the plot. 
@@ -27,7 +15,6 @@ def dbscan_cluster(smile, eps=0.2, min_samples=2, perplexity=5.0, learning_rate=
     Input to DBSCAN: 
     eps: The maximum distance between two points for them to be considered to belong in the same neighborhood.
     min_sample: The number of samples in a neighborhood for a point to be considered as a core point (including the point itself).
-
     """
     
     # Calculates fingerprints for all smiles.
@@ -50,24 +37,12 @@ def dbscan_cluster(smile, eps=0.2, min_samples=2, perplexity=5.0, learning_rate=
         bit_list.append(bits)
         smile_list.append(key)
 
-    #PCA on fingerprint data down to 50 dimensions or if that number is less than 50, to the number of fps.
-    data = np.array(bit_list)
-    if len(smile_list) >= 50:
-        n_components = 50
-    else:
-        n_components = len(bit_list)  
-    pca = PCA(n_components=n_components).fit_transform(data)
-
-    #tSNE on the resulting PCA data
-    data_embedded = TSNE(n_components=2, perplexity=perplexity, learning_rate=learning_rate,n_iter=n_iter).fit_transform(pca)
-    
     #DBSCAN, another clustering algoritm on the same data.
     clustering = DBSCAN(eps=eps, min_samples=min_samples, metric='jaccard').fit(np.array(bit_list))
-    print(f"The clustering labels are: {clustering.labels_}. \n Each color in the plot correspond to a specific cluster.")
-    dbscan_plot = plt.scatter(data_embedded[:, 0], data_embedded[:, 1], c=clustering.labels_, cmap='rainbow')
-
+    print(f"The clustering labels are: {clustering.labels_}. \nThe index of this list corresponds to the index of smile_list.")
+    
     #Generates a list that contains lists with all smiles that are in a cluster.
-    smile_cluster = []
+    dbscan_cluster = []
     for number in set(clustering.labels_):
         index = 0
         lista = []
@@ -75,6 +50,7 @@ def dbscan_cluster(smile, eps=0.2, min_samples=2, perplexity=5.0, learning_rate=
             if label == number:
                 lista.append(smile_list[index])
             index += 1
-        smile_cluster.append(lista)     
+        dbscan_cluster.append(lista)     
     
-    return dbscan_plot, smile_cluster
+    return dbscan_cluster
+    
